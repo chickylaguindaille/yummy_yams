db177729eb8c8cf8d35f8a641213423d4664a9c7
etienneleroywiboo <e.leroy@wiboo.fr>
2024-05-02 18:06:27 +0200

    essai pour récupérer les données mongodb

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 // Import des modules
 const express = require('express');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 require('dotenv').config(); // Pour charger les variables d'environnement
 
 // Initialisation de l'application Express
@@ -14,6 +14,35 @@ app.get('/', (req, res) => {
     res.send('Bienvenue sur votre backend !');
 });
 
+// Définition du modèle de données MongoDB
+// const DataModel = mongoose.model('yummy', new mongoose.Schema({
+//     name: String
+// }));
+
+const yummySchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    stock: Number,
+    quantityWon: Number
+});
+
+const DataModel = mongoose.model('yummy', yummySchema);
+
+
+// Route pour récupérer les données de la base de données
+app.get('/data', async (req, res) => {
+    try {
+        // Récupérer les données depuis la base de données
+        const data = await DataModel.find();
+        // const count = await DataModel.countDocuments();
+        // Renvoyer les données comme réponse
+        res.json(data);
+    } catch (error) {
+        // En cas d'erreur, renvoyer un message d'erreur
+        res.status(500).json({ message: error.message });
+    }
+});
+
 // Port d'écoute du serveur
 const PORT = process.env.PORT || 3000;
 
@@ -23,9 +52,9 @@ app.listen(PORT, () => {
 });
 
 // Connexion à MongoDB avec Mongoose
-// mongoose.connect(process.env.MONGODB_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// })
-// .then(() => console.log('Connexion à MongoDB réussie'))
-// .catch(err => console.error('Erreur de connexion à MongoDB :', err));
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Connexion à MongoDB réussie'))
+.catch(err => console.error('Erreur de connexion à MongoDB :', err));
